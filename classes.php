2680bacb2a9753290b54b7f997c685aba892b773
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-03-23 19:48:26 +0200

    room generator

--- classes.php
+++ classes.php
@@ -2,7 +2,14 @@
 class Room {
     public $id;
     public $type;
+    public $capacity;
     public $features = array_fill_keys(array("whiteboard", "audio", "projector"));
+    function __construct($type, $capacity, $features) {
+        $this->id = uniqid();
+        $this->type = $type;
+        $this->capacity = $capacity;
+        $this->features = $features;
+    }
 }
 class Booking {
     public $id;
---
+++ roomGenerator.php
@@ -0,0 +1,12 @@
+<?php
+require_once(classes.php)
+if($_GET['304'] == 303){
+    $features = array_fill_keys(array("whiteboard", "audio", "projector"));
+    $features['whiteboard'] = $_GET['whiteboard'];
+    $features['audio'] = $_GET['audio'];
+    $features['projector'] = $_GET['projector'];
+    $room = new Room($_GET['type'], $_GET['capacity'], $features);
+    $roomString = $room->id . "," . $room->type . "," . $room->capacity . "," . $room->features . PHP_EOL);
+    file_put_contents("data/rooms.csv", $roomString);
+}
+?>
