ca47195a1ef03d0cf1a1f03338ba29f02734ae94
Kristiina Šamanina <krsama@taltech.ee>
2021-03-23 20:35:14 +0200

    added validation

--- checkavailabilty.php
+++ checkavailabilty.php
@@ -1,2 +1,20 @@
 <?php
+if(isset($_GET['submit'])){
+    if(isset($_GET['start-date']) && isset($_GET['end-date']) && isset($_GET['capacity'])){
+        $date2 = DateTime::createFromFormat('Y-m-d', $_GET['end-date']);
+        $date1 = DateTime::createFromFormat('Y-m-d', $_GET['start-date']);
+        if($date1 === false){
+            exit("Please check Your date!");
+        }
+        else if($date2 === false){
+            exit("Please check Your date!");
+        }
+        else if($date2 < $date1){
+            exit("Please check Your date");
+        }
+        else if(!filter_var($_GET['capacity'], FILTER_VALIDATE_INT) === 0 || !filter_var($_GET['capacity'], FILTER_VALIDATE_INT)){
+            exit("Wrong input!");
+        }
+    }
+}
 ?>
