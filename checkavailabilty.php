171158a4041a6b1b16ff78c6c8ba202d195fa6c5
Edvin Ess <edvess@taltech.ee>
2021-03-23 20:47:31 +0200

    changed input to number in booking + added html to checkava + moved php into html

--- checkavailabilty.php
+++ checkavailabilty.php
@@ -1,20 +1,55 @@
-<?php
-if(isset($_GET['submit'])){
-    if(isset($_GET['start-date']) && isset($_GET['end-date']) && isset($_GET['capacity'])){
-        $date2 = DateTime::createFromFormat('Y-m-d', $_GET['end-date']);
-        $date1 = DateTime::createFromFormat('Y-m-d', $_GET['start-date']);
-        if($date1 === false){
-            exit("Please check Your date!");
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Contact & Feedback</title>
+        <link rel="shortcut icon" type="image/jpg" href="img/favicon.png" />
+        <link rel="stylesheet" href="styles/main.css">
+        <link href='https://fonts.googleapis.com/css?family=RocknRoll One' rel='stylesheet'>
+     </head>
+     <body>
+        <header>
+           <h1>Room booking</h1>
+           <ul id="login-menu">
+              <li><a href="login.html">Login</a></li>
+              <li><a href="register.html">Register</a></li>
+           </ul>
+        </header>
+        <nav>
+           <ul>
+              <li><a href="index.html">Home page</a></li>
+              <li><a href="overview.html">Overview</a></li>
+              <li><a href="booking.html">Booking</a></li>
+              <li><a href="contact.html">Contact</a></li>
+           </ul>
+        </nav>
+        <article>
+        <?php
+        if(isset($_GET['submit'])){
+            if(isset($_GET['start-date']) && isset($_GET['end-date']) && isset($_GET['capacity'])){
+                $date2 = DateTime::createFromFormat('Y-m-dTH%3i', $_GET['end-date']);
+                $date1 = DateTime::createFromFormat('Y-m-dTH%3i', $_GET['start-date']);
+                if($date1 === false){
+                    exit("Please check Your date! Incorrect start date!");
+                }
+                else if($date2 === false){
+                    exit("Please check Your date! Incorrect end date!");
+                }
+                else if($date2 < $date1){
+                    exit("Please check Your date! Starting date cannot be further than ending date!");
+                }
+                else if($_GET['start-date'] < date("Y-m-d")){
+                    exit("Please check Your date! Starting date cannot be in the past!");
+                }
+                else if(!filter_var($_GET['capacity'], FILTER_VALIDATE_INT) === 0 || !filter_var($_GET['capacity'], FILTER_VALIDATE_INT)){
+                    exit("Capacity must be a number (integer)!");
+                }
+                else{
+
+                }
+            }
         }
-        else if($date2 === false){
-            exit("Please check Your date!");
-        }
-        else if($date2 < $date1){
-            exit("Please check Your date");
-        }
-        else if(!filter_var($_GET['capacity'], FILTER_VALIDATE_INT) === 0 || !filter_var($_GET['capacity'], FILTER_VALIDATE_INT)){
-            exit("Wrong input!");
-        }
-    }
-}
-?>
+        ?>
+        </article>
+    </body>
+ </html>
