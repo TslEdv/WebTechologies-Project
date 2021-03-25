b37bc65ee09869d98a7648baac8eef7c4db612a9
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-03-25 21:32:44 +0200

    added more filtering functions

--- checkavailabilty.php
+++ checkavailabilty.php
@@ -28,9 +28,9 @@
         require_once("classes.php");
         if(isset($_GET['submit'])){
             if(isset($_GET['start-date']) && isset($_GET['end-date']) && isset($_GET['capacity'])){
-                $date2 = DateTime::createFromFormat('Y-m-dTH%3i', $_GET['end-date']);
+                $date2 = DateTime::createFromFormat('Y-m-dTH%3i', $_GET['end-date']); //converts date strings to dates
                 $date1 = DateTime::createFromFormat('Y-m-dTH%3i', $_GET['start-date']);
-                if($date1 === false){
+                if($date1 === false){ //input validation
                     exit("Please check Your date! Incorrect start date!");
                 }
                 else if($date2 === false){
@@ -42,24 +42,16 @@
                 else if($_GET['start-date'] < date("Y-m-d")){
                     exit("Please check Your date! Starting date cannot be in the past!");
                 }
-                else if(!filter_var($_GET['capacity'], FILTER_VALIDATE_INT) === 0 || !filter_var($_GET['capacity'], FILTER_VALIDATE_INT)){
+                else if(!filter_var($_GET['capacity'], FILTER_VALIDATE_INT) === 0 || !filter_var($_GET['capacity'], FILTER_VALIDATE_INT)){ //checks if capacity is integer
                     exit("Capacity must be a number (integer)!");
                 }
-                $capacity = intval($_GET['capacity']) * 2;
-                $whiteboard = intval($_GET['whiteboard']);
-                $audio = intval($_GET['audio']);
-                $projector = intval($_GET['projector']);
-                $featureSetArray = DataActions::readFeatures($capacity, )
-                $handle = fopen("data/rooms.csv", "r");
-                $roomtArray = array();
-                while ($data = fgetcsv($handle) !== FALSE){
-                    $room = new Room;
-                    $room->id = $data[0];
-                    $room->features = $data[1];
-                    array_push($roomtArray, $room);
-                }
-                fclose($handle);
-                
+                $capacity = intval($_GET['capacity']) * 2; //multiplied required capacity by 2 due to covid
+                $whiteboard = intval(isset($_GET['whiteboard'])); //converts required features into 0 or 1
+                $audio = intval(isset($_GET['audio']));
+                $projector = intval(isset($_GET['projector']));
+                $featureSetArray = DataActions::readFeatures($capacity, $whiteboard, $audio, $projector);
+                $roomArray = DataActions::readRooms($featureSetArray);
+                $bookingArray = DataActions::readBookings($roomArray, $date1, $date2);
             }
         }
         ?>
