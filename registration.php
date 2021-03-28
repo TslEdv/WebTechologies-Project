b48b8a2318a1feddb5d934bf2c6eed5d3a89ee67
Edvin Ess <edvess@taltech.ee>
2021-03-28 22:30:24 +0300

    added login features to registration.php

--- registration.php
+++ registration.php
@@ -3,8 +3,39 @@ if(isset($_POST['register'])){
     $password = password_hash($_POST['psw1'], PASSWORD_DEFAULT);
     unset($_POST['psw1']);
     if(!password_verify($_POST['psw2'], $password)){
-        echo "Password was unconfirmed! Please try again!";
+        exit("Password was unconfirmed! Please try again!");
     }
     unset($_POST['psw2']);
+    $text = $_POST['uname'] . "," . $password . PHP_EOL;
+    file_put_contents("data/users.csv", $text, FILE_APPEND);
+    echo "Registery Successful!";
+}
+if (isset($_POST['login'])){
+    $handle = fopen("data/users.csv", "r");
+    $success = FALSE;
+    while (($data = fgetcsv($handle)) !== FALSE){
+        if (($data[0] == $_POST['uname']) && (password_hash($_POST['psw1'], PASSWORD_DEFAULT) == $data[1])){
+            $success = TRUE;
+        }
+    }
+    if ($success == TRUE){
+        if(isset($_POST['remember'])){
+            session_name($_POST['uname']);
+            session_start();
+            $_SESSION['username'] = $_POST['uname'];
+            $_SESSION['id'] = session_id();
+            echo "Login Successful! Welcome back ", $_SESSION['username'];
+        } else{
+            session_name($_POST['uname']);
+            $lifetime=600;
+            session_set_cookie_params($lifetime);
+            session_start();
+            $_SESSION['username'] = $_POST['uname'];
+            $_SESSION['id'] = session_id();
+            echo "Login Successful! Welcome back ", $_SESSION['username'];
+        }
+    } else{
+        echo "Wrong Username or Password!";
+    }
 }
 ?>
