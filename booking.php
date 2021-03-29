1cd8cdedd0b80964db3a79cc1bc18bc201fc7a1d
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-03-29 15:19:55 +0300

    getters for booking.php

--- booking.php
+++ booking.php
@@ -1,13 +1,8 @@
 <?php
 require_once("classes.php");
-$booking = new Booking;
-$booking->id = uniqid();
-$booking->startDate = DateTime::createFromFormat('Y-m-dTH%3i', $_POST['end-date']);
-$booking->endDate = DateTime::createFromFormat('Y-m-dTH%3i', $_POST['end-date']);
-$booking->$roomId = $_POST['room-id'];
-$booking->user = $_POST['user'];
+$booking = new Booking(uniqid(), $_POST['user'],  DateTime::createFromFormat('Y-m-dTH%3i', $_POST['start-date']),  DateTime::createFromFormat('Y-m-dTH%3i', $_POST['end-date']), $_POST['roomid']);
 $handle = fopen("data/bookings.csv", "w");
 fputcsv($handle, get_object_vars($booking));
 fclose($handle);
-echo "Booking successful! Your booking number is " . $booking->id . "\n";
+echo "Booking successful! Your booking number is " . $booking->getId() . "\n";
 ?>
