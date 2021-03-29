07dfcf0ab6df5ba93fddc26480a0bfd911336030
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-03-29 15:12:52 +0300

    fixed room generator

--- roomGenerator.php
+++ roomGenerator.php
@@ -1,17 +1,11 @@
 <?php
 require_once("classes.php");
 if($_GET['304'] == 303){
-    $features = new FeatureSet;
-    $features->id = uniqid();
-    $features->capacity = $_GET['capacity'];
-    $features->whiteboard = $_GET['whiteboard'];
-    $features->audio = $_GET['audio'];
-    $features->projector = $_GET['projector'];
-    $featureString = $features->id . "," . $features->capacity . "," . $features->whiteboard . "," . $features->audio . "," . $features->projector . PHP_EOL;
+    $features = new FeatureSet(uniqid(), $_GET['capacity'], $_GET['whiteboard'], $_GET['audio'], $_GET['projector'], $_GET['title'], $_GET['description'], $_GET['image']);
+    $featureString = $features->getId() . "," . $features->getCapacity() . "," . $features->getWhiteboard() . "," . $features->getAudio() . "," . $features->getProjector() . "," . $features->getTitle() . "," . $features->getDescription() . "," . $features->getImage() . PHP_EOL;
     file_put_contents("data/featureset.csv", $featureString, FILE_APPEND);
-    $room = new Room($features);
-    $room->id = uniqid();
-    $roomString = $room->id . "," . $room->features->id . PHP_EOL;
+    $room = new Room(uniqid(), $features->getId());
+    $roomString = $room->getId() . "," . $room->getFeatures() . PHP_EOL;
     file_put_contents("data/rooms.csv", $roomString, FILE_APPEND);
 }
 else {
