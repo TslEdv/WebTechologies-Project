74e15f659c39c5028a265c6e5634fcaebb9eca33
Kristiina Šamanina <krsama@taltech.ee>
2021-04-10 16:40:08 +0300

    added mybooking.php, fixed bugs

---
+++ mybooking.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>My bookings</title>
+        <link rel="shortcut icon" type="image/jpg" href="img/favicon.png" />
+        <link rel="stylesheet" href="styles/main.css">
+        <link href='https://fonts.googleapis.com/css?family=RocknRoll One' rel='stylesheet'>
+     </head>
+     <body>
+        <header>
+           <h1>My bookings</h1>
+           <ul id="login-menu">
+              <li><a href="login.html">Login</a></li>
+              <li><a href="register.html">Register</a></li>
+           </ul>
+        </header>
+        <nav>
+           <ul>
+              <li><a href="index.html">Home page</a></li>
+              <li><a href="overview.html">Overview</a></li>
+              <li><a href="booking.html">Booking</a></li>
+              <li><a href="contact.html">Contact</a></li>
+           </ul>
+        </nav>
+        <article>
+        </article>
+    </body>
+ </html>
+
+ <?php
+   if()
+
+ ?>
