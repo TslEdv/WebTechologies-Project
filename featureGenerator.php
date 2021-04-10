d65047bf658a852ce575ec8fd4f9481db628d32d
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-04-10 17:17:58 +0300

    modified feature generator

--- featureGenerator.php
+++ featureGenerator.php
@@ -3,13 +3,14 @@ require_once("classes.php");
 if($_GET['304'] == 303){
     require_once("connect.db.php");
     $mysqli = new mysqli($db_server, $db_user, $db_password, $db_name);
-    $features = new FeatureSet(uniqid(), $_GET['capacity'], $_GET['whiteboard'], $_GET['audio'], $_GET['projector'], $_GET['title'], $_GET['description'], $_GET['image']);
-    $query = $mysqli->prepare("INSERT INTO featuresets (title, room_description, capacity, whiteboard, audio, projector) VALUES ?, ?, ?, ?, ? ?;");
-    $query->bind_param()
-    file_put_contents("data/featureset.csv", $featureString, FILE_APPEND);
-    $room = new Room(uniqid(), $features->getId());
-    $roomString = $room->getId() . "," . $room->getFeatures() . PHP_EOL;
-    file_put_contents("data/rooms.csv", $roomString, FILE_APPEND);
+    $query = $mysqli->prepare("INSERT INTO featuresets (title, room_description, image,  capacity, whiteboard, audio, projector) VALUES (?, ?, ?, ?, ? ?);");
+    $query->bind_param("ssiiii", $_GET['title'], $_GET['description'],  $_GET['image'], $_GET['capacity'], $_GET['whiteboard'], $_GET['audio'], $_GET['projector']);
+    $query->execute();
+    $lastId = $mysqli->insert_id;
+    $query = $mysqli->prepare("INSERT INTO rooms (feature_ID, room_number) VALUES ?, ?");
+    $query->bind_param("is", $lastId, $_GET['roomnumber']);
+    $query->close();
+    $mysqli->close();
 }
 else {
     exit("Invalid password");
