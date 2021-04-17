a5e3b785b0628b07578d597a41b1a6d909b7ec71
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-04-17 20:06:48 +0300

    password hashing bugfixes

--- registration.php
+++ registration.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Check availability</title>
+    <title>Registration</title>
     <link rel="shortcut icon" type="image/jpg" href="img/favicon.png" />
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/registration.css">
@@ -47,12 +47,12 @@
    </nav>
     <article>
         <?php
+        require_once("connect.db.php");
         if (isset($_POST['register'])) {
-            require_once("connect.db.php");
             $mysqli = new mysqli($db_server, $db_user, $db_password, $db_name); // connect to database
-            $username = DataActions::sanitiseInput($mysqli, $username); //sanitize
+            $username = mysqli_real_escape_string($mysqli, $_POST['uname']);
             $password = password_hash($_POST['psw1'], PASSWORD_BCRYPT);
-            if (empty($username) || empty($_POST['psw1']) || empty($_POST['psw2'])){ //check for inputs
+            if (empty($username) || empty($password) || empty($_POST['psw2'])){ //check for inputs
                 exit("Incorrect input!");
             }
             unset($_POST['psw1']); //unset post password
@@ -65,13 +65,16 @@
             $query->bind_param("s", $username);
             $query->execute();
             $query->bind_result($user);
+            print($query->error);
             if($query->fetch()){
                 exit("Username already exists.");
             }
+            print($password);
             $query = "INSERT INTO users (username, password) VALUES(?, ?)"; // query for adding user
             $query = $mysqli->prepare($query);
             $query->bind_param("ss", $username, $password);
             $query->execute();
+            print($query->error);
             session_name("PP_Table"); // start session and add variables
             session_start();
             $_SESSION['username'] = $username;
@@ -79,23 +82,19 @@
             echo "<p>Registery Successful!</p>";
         }
         else if (isset($_POST['login'])) { //check for login
-            require_once("connect.db.php");
             $mysqli = new mysqli($db_server, $db_user, $db_password, $db_name); // connect to database
-            $username = DataActions::sanitiseInput($mysqli, $_POST['uname']);
+            $username = mysqli_real_escape_string($mysqli, $_POST['uname']);
             if (empty($username)) {
                 exit("Username is required");
-            }
-            if (empty($password)) {
-                exit("Password is required");
             }
             $query = "SELECT * FROM users WHERE username=?"; //query for selecting the user from data
             $query = $mysqli->prepare($query);
             $query->bind_param("s", $username);
             $query->execute();
             $query->bind_result($user, $hash);
-            if($query->fetch()){
-                if(!password_verify($_POST['psw'], $hash)){
-                    exit("Password does not match");
+            while($query->fetch()){
+                if(password_verify($_POST['psw'], $hash)){
+                    break;
                 }
             }
             session_name("PP_Table");
